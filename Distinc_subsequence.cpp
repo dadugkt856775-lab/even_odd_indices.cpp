f915214e9b805cb563168280ac13ed41eb7f250c
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-09-22 21:36:35 +0530

    Add distinct subsequences counting algorithm
    Implement a solution to count distinct subsequences of two strings.

---
+++ Distinc_subsequence.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    int n = s.size();
+    int m = t.size();
+
+    vector<vector<long long>> dp(
+        n + 1,
+        vector<long long>(m + 1, 0)
+    );
+
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+
+            dp[i][j] = dp[i - 1][j];
+
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] += dp[i - 1][j - 1];
+        }
+    }
+
+    cout << "Number of Distinct Subsequences: "
+         << dp[n][m] << endl;
+
+    return 0;
+}
